ab99d7f87687d0546d776253d6f3f80f98280b72
Tapio Hallikainen <tapio.hallikainen@aalto.fi>
2016-07-17 13:44:37 +0300

    Refactored out the argument parser.

--- spcg.py
+++ spcg.py
@@ -5,15 +5,20 @@
 import argparse
 
 
-def main():
+# Parses command-line parameters and returns a dict of them
+def parse_parameters():
   parser = argparse.ArgumentParser(description='Parse script_file target_lang to optional output_file', epilog='An example command: python spcg.py script_file c out_file')
   parser.add_argument('script_file', help='A SPCG script file to generate the code from.')
   parser.add_argument('target_lang', help='The target language')
   parser.add_argument('output_file', nargs='?', default='a.out', help='The file for the generated code (default: a.out).')
 
-  args = parser.parse_args()
-  
-  d = vars(args)
+  args = parser.parse_args()  
+  return vars(args)
+
+# Generates a program code output to a file from a script file
+def main():
+  d = parse_parameters()
+
   print(d)
 #  print(d['output_file'])
 
